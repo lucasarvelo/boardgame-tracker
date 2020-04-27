4563512ec6b049de65045c2b02a5bc7c54c043ba
Lucas Arvelo <contact@lucasarvelo.com>
2020-04-27 19:00:28 -0400

    client/add MainImage component

---
+++ client/src/components/MainImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img :src="require(`@/assets/img/${filename}`)" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MainImage extends Vue {
+  @Prop({ default: 'image_url_fallback' })
+  filename!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  max-width: 100%;
+  max-height: 100%;
+  display: block; /* remove extra space below image */
+  border-radius: 25px;
+}
+
+div {
+  width: 50%;
+  align-self: center;
+  min-width: 410px;
+  padding: 15px;
+}
+</style>
