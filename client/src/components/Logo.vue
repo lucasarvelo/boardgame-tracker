f997c95506440f01901eab5cbb72f1608f5078cc
Lucas Arvelo <contact@lucasarvelo.com>
2020-04-27 18:53:01 -0400

    client/add logo component

---
+++ client/src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img class="logo" src="..\assets\img\Logo shadow.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Logo extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.logo {
+  width: 250px;
+  height: 58px;
+}
+</style>
