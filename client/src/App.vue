2276090651327f73c34132fdf00a963ded9d85ca
Lucas Arvelo <contact@lucasarvelo.com>
2020-04-27 18:58:54 -0400

    client/update add nav component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav />
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Logo from '@/components/Logo.vue';
+import Nav from '@/components/Nav.vue';
+
+@Component({
+  components: {
+    Logo,
+    Nav,
+  },
+})
+
+export default class App extends Vue {
+  isLogIn = false;
+}
+</script>
+
 <style lang="scss">
+@import "./assets/fonts/fonts.css";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: ibm_plex_sansregular, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #141414;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
