dfcaf10af28ea2261b80889f8a6e81faa8038063
Lucas Arvelo <contact@lucasarvelo.com>
2020-04-29 14:51:59 -0400

    fix styles

--- client/src/components/AboutBlock.vue
+++ client/src/components/AboutBlock.vue
@@ -21,12 +21,18 @@ export default class AuboutBlock extends Vue {
 
 <style lang="scss" scoped>
 div {
-    width: 45%;
+    width: 50%;
     margin: auto;
     text-align: justify;
 }
 
 p{
     font-weight: bold;
+}
+
+@media (max-width: 800px) {
+div{
+    width: 90%;
+}
 }
 </style>
