eda2bca924d5613081351c06a1198526f7c0208e
Lucas Arvelo <contact@lucasarvelo.com>
2020-04-29 10:55:56 -0400

    client/add AboutBlock component to Home

---
+++ client/src/components/AboutBlock.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <p>
+            Boardgame Tracker is a web app design to unify all about boardgames in one place.
+            Information about your games, tools for your game sessions and
+            the social side of boardgames in one stop make easy for the players
+            to enjoy the boardgame world and add value to our comunity.
+            If you have any question, sugestion or want to help,
+            get in touch with me at webcontact@lucasarvelo.com
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AuboutBlock extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    width: 45%;
+    margin: auto;
+    text-align: justify;
+}
+
+p{
+    font-weight: bold;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,6 +4,9 @@
       <SignUpForm isLogIn="false" />
       <MainImage filename="blood-rage-4311102_1280.jpg" />
     </div>
+    <div class="about">
+      <AboutBlock />
+    </div>
   </div>
 </template>
 
@@ -11,12 +14,14 @@
 // @ is an alias to /src
 import SignUpForm from '@/components/SignUpForm.vue';
 import MainImage from '@/components/MainImage.vue';
+import AboutBlock from '@/components/AboutBlock.vue';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component({
   components: {
     SignUpForm,
     MainImage,
+    AboutBlock,
   },
 })
 export default class SignupForm extends Vue {
@@ -31,5 +36,12 @@ export default class SignupForm extends Vue {
   flex-wrap: wrap-reverse;
   justify-content: space-evenly;
   align-items: center;
+  box-shadow: inset 0px -1px 0px #DEDEDE;
+  padding: 30px;
+}
+
+.about {
+  box-shadow: inset 0px -1px 0px #DEDEDE;
+  padding: 50px;
 }
 </style>
