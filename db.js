69ae000075f94486ce2bf82980d62d5c18c47aa8
Lucas Arvelo <contact@lucasarvelo.com>
2020-05-06 11:27:09 -0400

    add mongodb connection

---
+++ db.js
@@ -0,0 +1,31 @@
+module.exports = ({ url, port }, next) => {
+  const mongoose = require('mongoose');
+
+  mongoose
+    .connect('mongodb://' + url + ': ' + port + '/myapp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((error) => console.error(error.name + ': ' + error.message));
+
+  mongoose.connection.on('connected', function () {
+    console.log('Mongoose connection to ' + url + ':' + port + ' succeed!');
+  });
+
+  mongoose.connection.on('error', function (err) {
+    console.error('Mongoose connection ' + err.message + ' error');
+  });
+
+  mongoose.connection.on('disconnected', function () {
+    console.warn('Mongoose connection is disconnected');
+  });
+
+  process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+      console.log(
+        'Mongoose default connection is disconnected due to application termination',
+      );
+      process.exit(0);
+    });
+  });
+};
