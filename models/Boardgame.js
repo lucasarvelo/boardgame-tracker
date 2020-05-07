a7886452546b710483c2f96bd10e50b23183e3a4
Lucas Arvelo <contact@lucasarvelo.com>
2020-05-07 17:52:24 -0400

    add Boardgame model

---
+++ models/Boardgame.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const boardgameSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  designer: {
+    type: String,
+  },
+  yearpublished: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  minplayers: {
+    type: Number,
+  },
+  maxplayers: {
+    type: Number,
+  },
+  age: {
+    type: Number,
+  },
+  minPlaytime: {
+    type: Number,
+  },
+  maxPlaytime: {
+    type: Number,
+  },
+  boardgameHonors: {
+    type: Array,
+  },
+  boardgameMechanics: {
+    type: Array,
+  },
+});
+
+const Boardgame = mongoose.model('Boardgame', boardgameSchema);
+
+module.export = Boardgame;
