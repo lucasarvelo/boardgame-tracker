8218162dd21066df563392af44d98e58ec2dfa29
Lucas Arvelo <contact@lucasarvelo.com>
2020-05-07 17:51:57 -0400

    add xml parser

---
+++ utils/xmlParser.js
@@ -0,0 +1,17 @@
+const parseString = require('xml2js').parseString;
+
+const options = {
+  explicitArray: false,
+  mergeAttrs: true,
+};
+
+const xmlToJson = (xml) => {
+  let json;
+  parseString(xml, options, function (error, result) {
+    if (error) return error;
+    json = result;
+  });
+  return json;
+};
+
+module.exports = xmlToJson;
