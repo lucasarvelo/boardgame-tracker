75a468253d11e18b1df1bdbb4b3f6e91600b0757
Lucas Arvelo <contact@lucasarvelo.com>
2020-05-07 11:45:29 -0400

    fix error handle bug

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,6 @@
-module.exports = function errorHandler(err, req, res, next) {
-  if (res.headersSent) {
-    return next(err);
-  }
-  res.status(500);
-  res.render('error', { error: err });
+module.exports = function errorHandler(error, req, res, next) {
+  res.locals.error = error;
+  const status = error.status || 500;
+  res.status(status);
+  res.send(error);
 };
