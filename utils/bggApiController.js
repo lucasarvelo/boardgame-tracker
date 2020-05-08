e2f9a9dceca7a1f0f11ac13e6170922dd5503ef6
Lucas Arvelo <contact@lucasarvelo.com>
2020-05-08 16:19:00 -0400

    add search and boardgame info to BGG controller

--- utils/bggApiController.js
+++ utils/bggApiController.js
@@ -4,10 +4,35 @@ const xmlParser = require('../utils/xmlParser');
 const urlV1 = 'https://api.geekdo.com/xmlapi';
 const urlV2 = 'https://api.geekdo.com/xmlapi2';
 
-const getHotBoardgames = async () => {
-  return axios.get(urlV2 + '/hot?boardgame').then((response) => {
-    return xmlParser(response.data);
-  });
+const getHotBoardgames = () => {
+  return axios
+    .get(urlV2 + '/hot?boardgame')
+    .then((response) => xmlParser(response.data));
 };
 
-module.exports = { getHotBoardgames };
+const getBoardgameInformation = (id) => {
+  return axios
+    .get(urlV2 + '/thing?id=' + id)
+    .then((response) => {
+      return xmlParser(response.data);
+    })
+    .catch((error) => error);
+};
+
+const searchBoardgame = (query) => {
+  return axios
+    .get(urlV2 + '/search?type=boardgame,boardgameexpansion&query=' + query)
+    .then(async (response) => {
+      const parseSearchData = await xmlParser(response.data);
+
+      //get a list of boardgames ids from the search result to request more info
+      const boardgamesIds = parseSearchData
+        .map((game) => game.id.toString())
+        .join(',');
+
+      return getBoardgameInformation(boardgamesIds);
+    })
+    .catch((error) => error);
+};
+
+module.exports = { getHotBoardgames, searchBoardgame, getBoardgameInformation };
